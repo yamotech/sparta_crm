e3133f8216a083d39b60ee791a2938e2455b795f
yamotech <nothings.2c9@gmail.com>
2016-07-02 14:11:06 +0900

    ダミーデータの入れなおし

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Company.create(name: "YES高須クリニック", url: "http://www.takasu.co.jp/", address: "東京都港区赤坂2-14-27")
+Company.create(name: "株式会社ノーウォール", url: "http://nowall.co.jp/", address: "東京都新宿区西新宿6-15-1")
+Company.create(name: "ヤフー株式会社", url: "http://www.yahoo.co.jp/", address: "東京都港区赤坂9-7-1")
+
 100.times do |index|
   Customer.create(
     family_name: Faker::Japanese::Name.last_name,
     given_name: Faker::Japanese::Name.first_name,
-    email: "customer_#{index}@sparta.com"
+    email: "customer_#{index}@sparta.com",
+    company_id: rand(3) + 1
   )
 end
