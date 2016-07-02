de350d8b9e1ff873935de0a1b34c2138ef4b6530
yamotech <nothings.2c9@gmail.com>
2016-07-02 13:55:47 +0900

    companyモデルの実装

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701234442) do
+ActiveRecord::Schema.define(version: 20160702045246) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "url"
+    t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string   "family_name", null: false
