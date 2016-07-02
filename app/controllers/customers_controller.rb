283681739318c0045b8e921d7546a25bc2354cf7
yamotech <nothings.2c9@gmail.com>
2016-07-02 11:47:34 +0900

    update周りの作成

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -21,6 +21,12 @@ class CustomersController < ApplicationController
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to @customer
+    else
+      render :edit
+    end
   end
 
   def show
