7f9b6dc7d70e25491f3c6b3c85a0186ce08c55ec
yamotech <nothings.2c9@gmail.com>
2016-07-02 15:10:37 +0900

    Customerモデルにcompany_idのバリデーションを追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,7 @@ class Customer < ActiveRecord::Base
   validates :given_name,  presence: true, length: { maximum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,       presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :company_id,  presence: true
 
   def full_name
     family_name + given_name
