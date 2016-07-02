c116430308d7906cbaaccdbee40cada6afb32bef
yamotech <nothings.2c9@gmail.com>
2016-07-02 13:51:44 +0900

    gemの使用

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 100.times do |index|
-  Customer.create(family_name: "鈴木", given_name: "太郎", email: "customer_#{index}@sparta.com")
+  Customer.create(
+    family_name: Faker::Japanese::Name.last_name,
+    given_name: Faker::Japanese::Name.first_name,
+    email: "customer_#{index}@sparta.com"
+  )
 end
