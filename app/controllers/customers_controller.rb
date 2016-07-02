1aba08a73faacf9e0a623cf77e508c0c63e45faf
yamotech <nothings.2c9@gmail.com>
2016-07-02 17:52:55 +0900

    ログイン機能の実装、新規登録情報の追加途中

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,6 @@
 class CustomersController < ApplicationController
   before_action :set_customer, only: [ :show, :edit, :update, :destroy ]
+  before_action :authenticate_user!
 
   def index
     # @customers = Customer.page(params[:page])
