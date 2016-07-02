f59464e1f1bdf6cb2209c47e9a04a8f9dc362e1f
yamotech <nothings.2c9@gmail.com>
2016-07-02 17:00:48 +0900

    検索機能の実装

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,9 @@ class CustomersController < ApplicationController
   before_action :set_customer, only: [ :show, :edit, :update, :destroy ]
 
   def index
-    @customers = Customer.page(params[:page])
+    # @customers = Customer.page(params[:page])
+    @q = Customer.search(params[:q])
+    @customers = @q.result.page(params[:page])
   end
 
   def new
