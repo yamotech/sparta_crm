f29676fc61e0ca6e1fd749e6ce655ff829c982d9
yamotech <nothings.2c9@gmail.com>
2016-07-03 09:11:34 +0900

    comments#createメソッド開発

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,19 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to customer_url(@comment.customer_id)
+    else
+      redirect_to customer_url(@comment.customer_id)
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :customer_id)
   end
 end
