ee7cd3ac21496cede6aee7b5f67181ff0a864416
yamotech <nothings.2c9@gmail.com>
2016-07-03 09:28:12 +0900

    コントローラー改変

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -34,6 +34,8 @@ class CustomersController < ApplicationController
 
   def show
     @comment = Comment.new
+    # @comments = Comment.where(customer_id: params[:id].to_i)
+    @comments = @customer.comments
   end
 
   def destroy
