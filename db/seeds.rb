c0c19df581b430161084ed603cd8869b2a1efc38
yamotech <nothings.2c9@gmail.com>
2016-07-03 10:30:56 +0900

    viewの修正、seedデータの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,17 @@ Post.create(position_name: "部長")
 Post.create(position_name: "課長")
 Post.create(position_name: "平社員")
 
+User.create(family_name: "八幡平", given_name: "太郎", email: "hachiman@taro.com", password: "hachimantaro")
+User.create(family_name: "安比", given_name: "花子", email: "appi@hanako.com", password: "appihanako")
+User.create(family_name: "厚切り", given_name: "ダンソン", email: "atsugiri@danson.com", password: "atsugiridanson")
+
+300.times do |comment|
+  Comment.create(body: "#{comment}回目のコメントです。",
+                 customer_id: rand(100) + 1,
+                 user_id: rand(3) + 1)
+end
+
+
 100.times do |index|
   Customer.create(
     family_name: Faker::Japanese::Name.last_name,
